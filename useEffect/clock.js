96cd5f97c6da1dc46126de846f9873aaefabc92f
siddhi <87372462+sid161122@users.noreply.github.com>
2024-09-01 16:48:24 +0530

    Create clock.js

---
+++ useEffect/clock.js
@@ -0,0 +1,33 @@
+import React, {useState,useEffect  } from 'react';
+
+function App(){
+    const [time,setTime]=useState(new Date())
+
+    useEffect(()=>{
+        const interval=setInterval(()=>{
+            setTime(new Date())
+        },1000)
+
+        return()=>{
+            clearInterval(interval)
+        }
+    },[])
+  return (
+      <div>
+        <h1>Clock</h1>
+        <h1>{time.getHours().toString().padStart(2,'0')}</h1>
+        <h1>{time.getMinutes().toString().padStart(2,'0')}</h1>
+        <h1>{time.getSeconds().toString().padStart(2,'0')}</h1>
+
+          {time.toLocaleDateString(undefined,{
+          weekday:"long",
+          year:"numeric",
+          month:"long",
+          day:"numeric",
+          })}
+          
+      </div>
+  );
+}
+
+export default App;
